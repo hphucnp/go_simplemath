e910acd6c7a01749e031a576f3f632f54dec037b
phuc.nguyen <hphuc.np@gmail.com>
2025-08-08 16:22:50 +0700

    Add v2

--- v2/add.go
+++ v2/add.go
@@ -3,11 +3,18 @@
 // This package is designed for basic arithmetic functions.
 package gosimplemath
 
+import "golang.org/x/exp/constraints"
+
+// Number is a type constraint that matches any integer or floating-point type.
+type Number interface {
+    constraints.Integer | constraints.Float
+}
+
 // Add returns the sum of two numbers.
 //
 // This function adds the two parameters together and returns the result.
 // For more information about addition, see:
 // https://www.mathsisfun.com/numbers/addition.html
-func Add[T int](a, b int) int {
+func Add[T Number](a, b T) T {
     return a + b
 }
