8694cbb920ccd5ac86a498bf683a2d6aa119f94f
phuc.nguyen <hphuc.np@gmail.com>
2025-08-08 15:06:30 +0700

    ahihi

--- add.go
+++ add.go
@@ -1,5 +1,13 @@
+// Package gosimplemath provides simple mathematical operations.
+//
+// This package is designed for basic arithmetic functions.
 package gosimplemath
 
+// Add returns the sum of two integers.
+//
+// This function adds the two parameters together and returns the result.
+// For more information about addition, see:
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
